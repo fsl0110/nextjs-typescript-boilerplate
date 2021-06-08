e17809879dc512d3547bd19624a8df259df0807c
Frank Lechner <fsl011075@gmail.com>
2021-06-09 00:56:53 +0200

    cleaned up

--- .storybook/main.js
+++ .storybook/main.js
@@ -38,3 +38,13 @@ module.exports = {
     return config;
   },
 };
+/*Though the "loose" option was set to "false" in your @babel/preset-env config, 
+
+ it will not be used for  since the "loose" mode option was set to "true" for @babel/plugin-proposal-private-methods.
+The "loose" option must be the same for @babel/plugin-proposal-class-properties, @babel/plugin-proposal-private-methods and @babel/plugin-proposal-private-property-in-object (when they are enabled): you can silence this warning by explicitly adding
+        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+to the "plugins" section of your Babel config.
+
+@babel/plugin-proposal-private-property-in-object
+@babel/plugin-proposal-private-methods
+@babel/plugin-proposal-class-properties */
