32f026d8d9c29823908aa045bb3d6ca89b761651
Frank Lechner <fsl011075@gmail.com>
2021-06-08 19:52:44 +0200

    added and configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'sonarjs',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'comma-dangle': [2, 'always-multiline'],
+    'multiline-ternary': 0,
+    'semi': 0, // see @typescript-eslint/semi
+    'no-use-before-define': 0, // @typescript-eslint/no-use-before-define
+    'no-unused-vars': 0, // see @typescript-eslint/no-unused-vars
+    '@typescript-eslint/no-use-before-define': 2, // needs "no-use-before-define": 0,
+    '@typescript-eslint/semi': 2, // needs "semi": 0,
+    '@typescript-eslint/no-unused-vars': 2, // needs "no-unused-vars": 0,
+    '@typescript-eslint/member-delimiter-style': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    'react/prop-types': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'sonarjs/prefer-immediate-return': 0,
+    'sonarjs/cognitive-complexity': 1,
+    'sonarjs/no-duplicate-string': 0,
+    'prettier/prettier': 2,
+  },
+};
