d92024a2582a7a276124b4aa73ee0b76fcebf52a
Frank Lechner <fsl011075@gmail.com>
2021-06-09 09:26:24 +0200

    removed cypress component testing deps

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,9 @@
-describe('Feedback Test', () => {
+describe('Homepage Test', () => {
   before(() => {
     cy.visit('http://localhost:3000');
   });
 
-  it('should display headings', () => {
+  it('should display content', () => {
     cy.get('h1').should('be.visible').and('contain', 'Welcome to Next.js!');
     cy.get('.main')
       .should('be.visible')
